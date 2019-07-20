df4a18d9f87567372582bf14a1870245df659339
Ömer Burak KARATAŞ <omer.burak.karatas@outlook.com>
2019-07-20 18:36:59 +0300

    Button component
    Create initial button component
    - Add IButton interface
    - update reset css

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import IButton from '../../common/types/components/IButton';
+
+const Button: React.FunctionComponent<IButton> = (props) => {
+	return <button onClick={props.onClick}>{props.label}</button>;
+};
+
+export default Button;
