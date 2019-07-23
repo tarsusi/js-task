08cd408a078ecf35fc4145f9783046ab9a4c657b
Ömer Burak KARATAŞ <omer.burak.karatas@outlook.com>
2019-07-23 20:49:32 +0300

    Testing Footer component
    - complete test case for footer component

--- src/views/layout/footer/Footer.test.tsx
+++ src/views/layout/footer/Footer.test.tsx
@@ -1,6 +1,15 @@
-// ETA: 30 minutes
+import * as React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Footer from './Footer';
+
+afterEach(cleanup);
+
 describe('<Footer />', () => {
 	it('should render "© AUTO1 Group 2019"', () => {
-		expect(false).toBe(true);
+		const { getByText } = render(<Footer />);
+		expect(getByText('© AUTO1 Group 2019')).toBeInTheDocument();
 	});
 });
